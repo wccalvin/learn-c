1db0fb37fef7ac2e6e09889260a87f64688c7e87
Clayton William <clayton@wccalvin.com>
2023-12-28 20:09:23 -0500

    add puts example

--- 01-hello/hello.c
+++ 01-hello/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello, world!\n");
-    return 0;
+  printf("hello, world!\n");
+  puts("this prints the content and inserts a new line at the end");
+  return 0;
 }
