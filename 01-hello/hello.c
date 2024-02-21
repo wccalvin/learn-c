6182d273d75f71664f5c701c166d8913a7ad8c56
wccalvin <clayton@wccalvin.com>
2024-02-20 21:57:03 -0500

    back to basics

--- 01-hello/hello.c
+++ 01-hello/hello.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
   printf("hello, world!\n");
   puts("this prints the content and inserts a new line at the end");
-  return EXIT_SUCCESS;
+  return 0;
 }
