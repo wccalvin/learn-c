57c1e832e1bd255c05b76f5510d30b270f18e2dc
wccalvin <clayton@wccalvin.com>
2024-02-21 22:03:46 -0500

    example for argc and argv

---
+++ 02-helloArgs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// another legal signature of main
+int main(int argc, char **argv) {
+  // argc is an automatically calculated value -> represents total number of
+  // arguments stored in argv
+  if (argc == 1)
+    printf("hello, world.\n");
+  else {
+    // `char **argv` is same as `char *argv[]`
+    // it represents array of pointers to a string
+    // in other words, each element of array is a pointer which points to a
+    // string
+    // printf("argc=%d\n", argc);
+    for (int i = 0; i < argc; ++i)
+      printf("argument: %d and value: %s\n", i, argv[i]);
+  }
+  return 0;
+}
+
+// execute:
+// ❯ gcc -o a 02-helloArgs.c
+// ❯ ./a this is fun
+// argument: 0 and value: ./a
+// argument: 1 and value: this
+// argument: 2 and value: is
+// argument: 3 and value: fun
