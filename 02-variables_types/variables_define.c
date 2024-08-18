ae64afea6f4bd1d954b25a77626d64c4e676acaa
wccalvin <clayton@wccalvin.com>
2024-08-17 21:26:29 -0400

    example of global variables using macro redefinition

--- 02-variables_types/variables_define.c
+++ 02-variables_types/variables_define.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+// these are instructions to pre-processor.
+// the values are substituted instead of assignment
 #define PI 3.141593
   printf("defined constant variable PI = %lf\n", PI);
+// macro redefinition is possible - it compiles with warnings
+#define PI 3.14
+  printf("defined constant variable PI = %.2lf\n", PI);
   return 0;
 }
